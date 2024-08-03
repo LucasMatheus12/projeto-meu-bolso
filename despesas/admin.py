5fcd600f2c463a299c4232fbcb064b1779ffb36b
unknown <feitozaac@gmail.com>
2024-08-03 18:18:26 -0300

    <fix> [Correção de erros] <correção de erros na model>

--- despesas/admin.py
+++ despesas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Categoria, Despesa
 # Register your models here.
+
+
+admin.site.register(Categoria)
+admin.site.register(Despesa)
--- despesas/models.py
+++ despesas/models.py
@@ -9,12 +9,13 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome
 
-Class Despesa(models.Model):
+class Despesa(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True,null=True)
     data = models.DateField()
     categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
     valor = models.DecimalField(max_digits=10,decimal_places=2)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
-     def __str__(self):
+    def __str__(self):
         return self.nome
