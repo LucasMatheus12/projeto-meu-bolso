f41c911e0986258e19b11b2d253dd148b86c94ca
unknown <feitozaac@gmail.com>
2024-08-03 18:04:10 -0300

    <feat> [model Categoria] <Adicionando model de despesa em despesas

--- despesas/models.py
+++ despesas/models.py
@@ -9,3 +9,12 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nome
 
+Class Despesa(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True,null=True)
+    data = models.DateField()
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10,decimal_places=2)
+
+     def __str__(self):
+        return self.nome
