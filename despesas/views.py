524666bd18079e36cd42545ee691b57d8ec199aa
LucasMatheus12 <matheusalexandre012@gmail.com>
2024-08-04 21:01:21 -0300

    <feat> [View Categoria] <Adicionando view de categoria>

--- despesas/views.py
+++ despesas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Categoria, Despesa
 # Create your views here.
+class GerenciarCArtegoriaView(View): 
+    template_name= "despesas/gerenciar_categoria.html"
+    def get(self, request):
+        categorias= Categoria.objects.filter(usuario=request.user)
+        context = { 
+            'categorias',categorias
+        }
+        return render(request,self.template_name, context)
