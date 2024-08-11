920496cfe72cfe9ffc1756958080449cc48980be
unknown <feitozaac@gmail.com>
2024-08-11 18:58:21 -0300

    <feat> [Criando listagemd de despesas] <adicionando guia e tabela>

--- despesas/urls.py
+++ despesas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import GerenciarCategoriaView
+from .views import GerenciarCategoriaView,ListaDespesa
 
 urlpatterns=[
     path('categorias/',view=GerenciarCategoriaView.as_view(),name='gerenciar_categorias'),
--- despesas/views.py
+++ despesas/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views import View
 from .models import Categoria, Despesa
 # Create your views here.
+class ListaDespesas(View):
+    template_name='despesas/lista_despesas.html'
+    def get(self,request):
+        despesas = Despesa.object.filter(usuario = request.user)
+        return render(request,self.template_name,{'despesas':despesas})
 class GerenciarCategoriaView(View):
     template_name= 'despesas/gerenciar_categorias.html'
     def get(self,request):
