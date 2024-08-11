6c1c980870b12461052c8b075cc7303f995f90de
LucasMatheus12 <matheusalexandre012@gmail.com>
2024-08-11 20:32:55 -0300

    <feat> [Criando rota para o post de formulário de categorias] <adicionando post em gerenciar categorias>

--- despesas/urls.py
+++ despesas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import GerenciarCategoriaView,ListaDespesa
+from .views import GerenciarCategoriaView,ListaDespesasView
+
 
 urlpatterns=[
     path('categorias/',view=GerenciarCategoriaView.as_view(),name='gerenciar_categorias'),
+    path('despesas/',view=ListaDespesasView.as_view(),name='lista_despesas'),
 ]
