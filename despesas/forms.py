db72ab030390b6b88f72de1f72ab5487dee861b5
LucasMatheus12 <matheusalexandre012@gmail.com>
2024-10-06 18:07:35 -0300

    <feat> [form deposito] <Formulário para Adicionar Depósito>

--- despesas/forms.py
+++ despesas/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Categoria,Despesa
+from .models import Categoria,Despesa,Deposito
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
@@ -37,3 +37,12 @@ class DespesaForm(forms.ModelForm):
     def __init__(self,user,*args,**kwargs):
         super(DespesaForm,self).__init__(*args,**kwargs)
         self.fields['categoria'].queryset= Categoria.objects.filter(usuario=user)
+
+class DepositoForm(forms.ModelForm):
+    class Meta:
+        model = Deposito
+        fields = ['categoria', 'valor', 'data', 'descricao']
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['categoria'].queryset = Categoria.objects.filter(usuario=user)
