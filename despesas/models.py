501b473cc75a8268126e55e6cc2a54d00b766eba
LucasMatheus12 <matheusalexandre012@gmail.com>
2024-10-06 18:05:01 -0300

    <feat> [model depposito] <Adicionando model de deposito>

--- despesas/models.py
+++ despesas/models.py
@@ -19,3 +19,13 @@ class Despesa(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Deposito(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.descricao} - {self.valor}"
